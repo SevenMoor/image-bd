929bf340bb583adae53d35d0f516994c7125198e
Vykad <alban.lemoult@gmail.com>
2020-09-21 14:51:48 +0200

    Correction du main

--- features.cpp
+++ features.cpp
@@ -6,14 +6,17 @@ int main(int argc, char const *argv[])
 {
 	long nrl, nrh, ncl, nch;
 
+	// std::cout << "Chargement de l'image" << std::endl;
+
 	char path[100] = IMAGE_PATH;
 	strcat(path, "arbre1.pgm");
 	byte **matrix = LoadPGM_bmatrix(path, &nrl, &nrh, &ncl, &nch);
 
-	strcpy(path, IMAGE_PATH);
-	strcat(path, "arbre1bis.pgm");
+	// std::cout << "Sauvegarde de la copie de l'image" << std::endl;
+	strcpy(path, "arbre1bis.pgm");
 	SavePGM_bmatrix(matrix, nrl, nrh, ncl, nch, path);
 
+	// std::cout << "Libération des matrices" << std::endl;
 	free_bmatrix(matrix, nrl, nrh, ncl, nch);
 
 	return 0;
