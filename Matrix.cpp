8fd9552da6c6e3f8ccff2e648f7d818a4c273f2b
SevenMoor <au.ottaviano@yahoo.com>
2020-09-21 15:48:21 +0200

    Completed matrix members

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,25 +1,73 @@
 #include "Matrix.h"
 
 int Matrix::height(){
-
+    return _height;
 }
 
 int Matrix::width(){
-
+    return _width;
 }
 
 int** Matrix::content(){
+    return _content;
+}
 
+void Matrix::setContent(int** content){
+    _content = content;
 }
 
 int* Matrix::operator[](int i){
-
+    return *_content;
 }
 
-Matrix Matrix::sobel(){
-
+Matrix::Matrix(int width, int height) : _width(width), _height(height) {
+    _content = (int**) malloc(width*height*sizeof(int));
 }
 
-Matrix::Matrix ones(){
+Matrix* Matrix::verticalSobel(){
+    int** tmp = (int**) malloc(3*3*sizeof(int));
+    tmp[0][0] = -1;
+    tmp[0][1] = 0;
+    tmp[0][2] = 1;
+    tmp[1][0] = -2;
+    tmp[1][1] = 0;
+    tmp[1][2] = 2;
+    tmp[2][0] = -1;
+    tmp[2][1] = 0;
+    tmp[2][2] = 1;
+    Matrix* m = new Matrix(3,3);
+    m->setContent(tmp);
+    return m;
+}
 
+Matrix* Matrix::verticalSobel(){
+    int** tmp = (int**) malloc(3*3*sizeof(int));
+    tmp[0][0] = -1;
+    tmp[0][1] = -2;
+    tmp[0][2] = -1;
+    tmp[1][0] = 0;
+    tmp[1][1] = 0;
+    tmp[1][2] = 0;
+    tmp[2][0] = 1;
+    tmp[2][1] = 2;
+    tmp[2][2] = 1;
+    Matrix* m = new Matrix(3,3);
+    m->setContent(tmp);
+    return m;
+}
+
+Matrix::~Matrix(){
+    free(_content);
+}
+
+Matrix* Matrix::ones(){
+    int** tmp = (int**) malloc(3*3*sizeof(int));
+    for (int i=0; i<3; i++){
+        for (int j=0; j<3; j++){
+            tmp[i][j] = 1;
+        }
+    }
+    Matrix* m = new Matrix(3,3);
+    m->setContent(tmp);
+    return m;
 }
