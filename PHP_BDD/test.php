49b5921c759b0342ce571500b10bc2dfae189a8e
AlbanLM <alban.lemoult@gmail.com>
2020-09-25 12:55:36 +0200

    Auto stash before merge of "master" and "origin/master"

--- PHP_BDD/test.php
+++ PHP_BDD/test.php
@@ -1,22 +1,18 @@
 <?php
 	include 'OCIConnection.php';
 
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+
 	$OCICon = OCIConnection::getConnection();
-	if (!$OCICon) {
-		print 'oci = null<br/>';
-	}
-	else {
-		print 'oci != null<br/>';
-		if (!$OCICon->_connection) {
-			print 'connection = null<br/>';
-		}
-		else {
-			print 'connection != null<br/>';
-		}
+	var_dump($OCICon);
 
-	}
-
-	$parsed = oci_parse($OCICon->_connection, "select * from similaire order by dist asc;");
+	echo '<br/><br/>Requete <br/>';
+	$parsed = oci_parse($OCICon->_connection, "begin ranker_(:p); end;");
+	$jpg = "1.jpg";
+	oci_bind_by_name($parsed,':p', $jpg, 50, SQLT_CHR);
 	var_dump(oci_execute($parsed));
 
+	var_dump(oci_error());
 ?>
