87e844f5c591ebf478ff209e096f1b8074320992
AlbanLM <alban.lemoult@gmail.com>
2020-09-25 10:18:54 +0200

    Fixed again DB Connection

--- PHP_BDD/OCIConnection.php
+++ PHP_BDD/OCIConnection.php
@@ -5,7 +5,8 @@
 		* @access private
 		* @static
 		*/
-		private static $_connection = null;
+		private static $_ociConnection = null;
+		public $_connection = null;
 
 		/**
 		*
@@ -14,9 +15,9 @@
 		* @param void
 		* @return void
 		*/
-		private function __construct() {  
-			$conn = oci_connect("Y2020M2_PAUQUET", "M123456M", "//10.40.128.30:1521/orcl");
-/*			if (!$conn) {
+		private function __construct() {
+			$this->_connection = oci_connect("Y2020M2_PAUQUET", "M123456M", "//10.40.128.30:1521/orcl");
+			/*if (!$this->_connection) {
 				$m = oci_error();
 				echo $m['message'], "\n";
 				exit;
@@ -27,7 +28,9 @@
 		}
 
 		private function __destruct() {
-			oci_close(self::$_connection);
+			if ($this->_connection) {
+				oci_close($this->_connection);
+			}
 		}
 
 		/**
@@ -38,11 +41,10 @@
 		* @return OCIConnection
 		*/
 		public static function getConnection() {
-			if(is_null(self::$_connection)) {
-				self::$_connection = new OCIConnection();
+			if(is_null(self::$_ociConnection)) {
+				self::$_ociConnection = new OCIConnection();
 			}
-
-			return self::$_connection;
+			return self::$_ociConnection;
 		}
 	}
 ?>
--- PHP_BDD/test.php
+++ PHP_BDD/test.php
@@ -1,5 +1,22 @@
 <?php
 	include 'OCIConnection.php';
 
-	$connection = OCIConnection::getConnection();
+	$OCICon = OCIConnection::getConnection();
+	if (!$OCICon) {
+		print 'oci = null<br/>';
+	}
+	else {
+		print 'oci != null<br/>';
+		if (!$OCICon->_connection) {
+			print 'connection = null<br/>';
+		}
+		else {
+			print 'connection != null<br/>';
+		}
+
+	}
+
+	$parsed = oci_parse($OCICon->_connection, "select * from similaire order by dist asc;");
+	var_dump(oci_execute($parsed));
+
 ?>
